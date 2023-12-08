60d15de945828cb91af3dc2c853152381c7e74ff
charlieboardman <cboardman2@gmail.com>
2023-12-07 20:07:35 -0800

    Progress on d7p1

--- 2023/7/main1.py
+++ 2023/7/main1.py
@@ -14,3 +14,74 @@ for hand in hands:
     breakdown = [[x[0],int(x[1])] for x in breakdown]
     
     hand.append(breakdown)
+    
+for hand in hands:
+    
+    length_breakdown = len(hand[2])
+    
+    if length_breakdown == 1: #5 of a kind
+        hand.append('A')
+        continue
+    
+    if length_breakdown == 2: #Full house or 4 or a kind:
+        if hand[2][0][1] in range(2,4): #if 2 or 3, full house
+            hand.append('C')
+            continue
+        else: #4 of a kind
+            hand.append('B')
+            continue
+    
+    if length_breakdown == 3: #3 of a kind or 2 pair
+        if 3 in [x[1] for x in hand[2]]: #3 of a kind
+            hand.append('D')
+            continue
+        else: #2 pair
+            hand.append('E')
+            continue
+    
+    if length_breakdown == 4: #2 of a kind
+        hand.append('F')
+        continue
+    
+    if length_breakdown == 5: #High card
+        hand.append('G')
+        continue
+    
+A_hands = [hand for hand in hands if hand[3] == 'A']
+B_hands = [hand for hand in hands if hand[3] == 'B']
+C_hands = [hand for hand in hands if hand[3] == 'C']
+D_hands = [hand for hand in hands if hand[3] == 'D']
+E_hands = [hand for hand in hands if hand[3] == 'E']
+F_hands = [hand for hand in hands if hand[3] == 'F']
+G_hands = [hand for hand in hands if hand[3] == 'G']
+
+card_str= {'2':0,'3':1,'4':2,'5':3,'6':4,'7':5,'8':6,'9':7,'T':8,'J':9,'Q':10,'K':11,'A':12}
+
+def compare_hands(hand_a,hand_b): #Returns true if a > b, false if b > a
+    n_cards = 5 #5 cards in a hand
+    for n in range(n_cards):
+        if card_str[hand_a[0][n]] > card_str[hand_b[0][n]]: #hand_a win
+            return True
+        
+        if card_str[hand_a[0][n]] < card_str[hand_b[0][n]]: #hand_b win
+            return False
+        
+        else:
+            continue
+        
+def hands_sort(hand_group):
+    
+    n_hands = len(hand_group)
+    n = 0
+    
+    while n < n_hands - 1:
+        if compare_hands(hand_group[n],hand_group[n+1]):
+            swap = hand_group[n]
+            hand_group[n] = hand_group[n+1]
+            hand_group[n+1] = swap
+            n = 0
+            continue
+        n += 1
+        
+        
+hands_sort(B_hands) 
