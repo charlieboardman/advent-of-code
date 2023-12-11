b2b0128045e35da669f57ef8fee337bbfdd1fba6
Charlie Boardman <cboardman2@gmail.com>
2023-12-11 12:00:38 -0800

    d10p1 complete

--- 2023/10/main1.py
+++ 2023/10/main1.py
@@ -5,22 +5,23 @@ with open('input.txt','r') as pipe_map:
 
 #Find the starting point
 def find_start(array):
-    for row in range(len(array)):
-        for col in range(len(array[row])):
-            if array[row][col] == 'S':
-                return row,col
-            
+    for n,row in enumerate(array):
+        if 'S' in row:
+            for col in range(len(row)):
+                if row[col] == 'S':
+                    return n,col
+                
 #Initialize the array of distances
 dists = []
 for i in range(len(array)):
     dists.append(list(np.zeros(len(array[0]))))
 
-#Above, below, right, left
-connects = [['|','7','F','S'],['|','L','J','S'],['-','L','F','S'],['-','J','7','S']]
+#Right, above, left, below
+connects_to_center = [['-','J','7'],['|','7','F'],['-','L','F'],['|','L','J']]
 
 row,col = find_start(array)
 
-def find_adj_connectors(row,col):
+def find_adj(row,col):
     if row != 0:
         above = array[row-1][col]
     else:
@@ -41,46 +42,122 @@ def find_adj_connectors(row,col):
     else:
         left = ''
         
-    return [above,below,right,left]
+    return [right,above,left,below]
 
 def are_connectors(adj):
     ans = []
     for n in range(len(adj)):
-        ans.append(adj[n] in connects[n])
+        ans.append(adj[n] in connects_to_center[n])
     return ans
 
-next_tile = ''
-tile = 'S'
+def determine_coming(row,col,prev_row,prev_col):
+    if prev_col == col+1:
+        return 0
+    if prev_row == row-1:
+        return 1
+    if prev_col == col-1:
+        return 2
+    if prev_row == row+1:
+        return 3
+
+def router(tile,coming):
+    if tile == '|':
+        if coming == 1:
+            return 3
+        else:
+            return 1
+        
+    if tile == '-':
+        if coming == 2:
+            return 0
+        else:
+            return 2
+        
+    if tile == '7':
+        if coming == 2:
+            return 3
+        else:
+            return 2
+        
+    if tile == 'F':
+        if coming == 0:
+            return 3
+        else:
+            return 0
+        
+    if tile == 'L':
+        if coming == 1:
+            return 0
+        else:
+            return 1
+        
+    if tile == 'J':
+        if coming == 1:
+            return 2
+        else:
+            return 1
+
+#Process the first tile outside the loop since we have to arbitrarily find coming and going
 
 distance = 0
 
+#Process the current tile
+dists[row][col] = distance
+
 #We have to choose a direction to start. Arbitrarily pick that we are coming from the first True and going to the 2nd True in connectors
-connectors = are_connectors(find_adj_connectors(row,col))
+connectors = are_connectors(find_adj(row,col))
 coming = [i for i,n in enumerate(connectors) if n == True][0]
 going = [i for i,n in enumerate(connectors) if n == True][1]
 
-while next_tile != 'S':
+prev_row = row
+prev_col = col
+
+if going == 0:
+    col = col+1
+    
+if going == 1:
+    row = row-1
+
+if going == 2:
+    col = col-1
+
+if going == 3:
+    row = row+1 
+
+tile = array[row][col]
+distance += 1
+
+while dists[row][col] == 0:
+    
+    #print(tile)
+    if distance == 23:
+        pass
     
     dists[row][col] = distance
 
-    from_row = row
-    from_col = col
+    coming = determine_coming(row,col,prev_row,prev_col)
+    going = router(tile,coming)
+    
+    connectors = are_connectors(find_adj(row,col))
+
+    prev_row = row
+    prev_col = col
 
     if going == 0:
-        row = row-1
+        col = col+1
     
     if going == 1:
         row = row-1
     
     if going == 2:
-        col = col+1
-    
-    if going == 3:
         col = col-1
     
-    connectors = are_connectors(find_adj_connectors(row,col)):
-        going = [i for i,n in enumerate(connectors) if n == True and    
+    if going == 3:
+        row = row+1
     
-    next_tile = array[row][col]
-    
-    
+    tile = array[row][col]
+
+    distance += 1
+
+
+print(dists[row][col]//2)
