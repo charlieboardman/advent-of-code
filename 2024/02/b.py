19f8dedce41ba4a22b06d16274d97014bc34a9b5
Charlie Boardman <cboardman2@gmail.com>
2024-12-02 16:36:42 -0800

    Got b working, which made me make some changes to a as well for DRY

--- 2024/02/b.py
+++ 2024/02/b.py
@@ -9,13 +9,20 @@ def report_is_safe(report):
 
 reports = []
 safes = []
+
 with open('input') as f:
     for line in f:
         report = [int(n) for n in line.split()]
-        report_diff = np.diff(report)
         if report_is_safe(report):
             safes.append(1)
+            continue
         else:
+            for i in range(len(report)):
+                modded_report = report[:i] + report[i+1:]
+                modded_report_diff = np.diff(modded_report)
+                if report_is_safe(modded_report):
+                    safes.append(1)
+                    break
             safes.append(0)
-            
+
 print(sum(safes))
