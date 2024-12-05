7b15298ece3d5ded995d8a371b7bd083469a1295
Charlie Boardman <cboardman2@gmail.com>
2024-12-05 01:16:33 -0800

    Day 5 part 1 complete

---
+++ 2024/05/a.py
@@ -0,0 +1,14 @@
+rules = [(int(line.split('|')[0]),int(line.split('|')[1])) for line in open('input') if '|' in line]
+updates = [[int(x) for x in line.split(',')] for line in open('input') if ',' in line]
+
+correct_updates = [x for x in updates] #We will remove the bad ones from this list. List construction so as not to share memory address
+for update in updates:
+    for rule in rules:
+        if rule[0] in update and rule[1] in update:
+            if update.index(rule[0]) < update.index(rule[1]):
+                continue
+            else:
+                correct_updates.remove(update)
+                break
+                
+print(sum([update[int(len(update)/2)] for update in correct_updates]))
