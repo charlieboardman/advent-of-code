996bd3ca3a0195943479d2e489e66cf1a589e9cf
Charlie Boardman <cboardman2@gmail.com>
2024-12-06 01:08:48 -0800

    Day 6 part 1 complete, wrote out a while loop with if statements that walks through the logic described in the description, leaving a mark at each spot visited. Then add those up

---
+++ 2024/06/a.py
@@ -0,0 +1,53 @@
+labmap = [line.strip('\n') for line in open('input')]
+
+#Find start
+def find_origin(labmap):
+    for i in range(len(labmap)):
+        for j in range(len(labmap[0])):
+            if labmap[i][j] in '<>^v':
+                return (i,j,labmap[i][j])
+
+origin = find_origin(labmap)
+[i,j,d] = [x for x in origin]
+
+#Build empty map, preallocating zeros
+visited = []
+for x in range(len(labmap)):
+    visited.append([])
+    for y in range(len(labmap[0])):
+        visited[x].append(0)
+
+
+while 0 <= i < len(labmap) and 0 <= j < len(labmap[0]):
+    visited[i][j] = 1
+    #Look ahead
+    if d == '^':
+        if labmap[i-1][j] == '#':
+            d = '>'
+            continue
+        else:
+            i = i-1
+            continue
+    if d == '>':
+        if labmap[i][j+1] == '#':
+            d = 'v'
+            continue
+        else:
+            j = j+1
+    if d == 'v':
+        if labmap[i+1][j] == '#':
+            d = '<'
+            continue
+        else:
+            i = i+1
+    if d == '<':
+        if labmap[i][j-1] == '#':
+            d = '^'
+            continue
+        else:
+            j = j-1
+
+
+total = sum([sum(row) for row in visited])
+    
+print(total)
